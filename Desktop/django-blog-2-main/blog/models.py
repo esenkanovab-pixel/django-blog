f8827e610bb7a9708826e12e4595f16755270ec6
esenkanovab-pixel <esenkanov_ab@iuca.kg>
2025-10-06 17:00:25 +0600

    Добавлены категории в модели, улучшен интерфейс с Bootstrap, добавлены кнопки и стиль

--- Desktop/django-blog-2-main/blog/models.py
+++ Desktop/django-blog-2-main/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Модель категории
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
@@ -8,14 +9,13 @@ class Category(models.Model):
         return self.name
     
 
+# Модель поста
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, blank=True)  # связь с категориями
+    created_at = models.DateTimeField(auto_now_add=True)        # дата создания
 
     def __str__(self):
         return self.title
-
-
