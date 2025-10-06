2d9538ea6b33965e5991b0e83fee099bfe60c98f
esenkanovab-pixel <esenkanov_ab@iuca.kg>
2025-10-06 16:05:49 +0600

    Добавлены кнопки Удалить и Назад для поста, подготовка к стилизации

---
+++ Desktop/django-blog-2-main/blog/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from .models import Post
+from .forms import PostForm
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            form.save_m2m()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_form.html', {'form': form})
+
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            # post = form.save(commit=False)
+            # post.author = request.user
+            # post.save()
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_form.html', {'form': form})
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('post_list')  # После удаления возвращаемся к списку постов
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
+# C_R_UD
+
+# Post.objects.all()
+# Post.objects.get(id=1)
+# Post.objects.filter()
+# author = User.objects.get(username='pushkin')
+# Filters :
+    # contains 
+    # exact =
+    # startwith
+    # endwith 
+    # gt >
+    # gte >=
+    # lt < 
+    # lte <=
